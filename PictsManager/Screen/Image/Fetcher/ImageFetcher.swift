14fbcc0a5e87097f07664798cbf395ccc70a4ad5
minhduc <ducbv1407@gmail.com>
2024-04-15 16:33:54 +0200

    fix: assign imageData in main thread

--- PictsManager/Screen/Image/Fetcher/ImageFetcher.swift
+++ PictsManager/Screen/Image/Fetcher/ImageFetcher.swift
@@ -9,26 +9,28 @@ import Foundation
 import SwiftUI
 
 class ImageFetcher: ObservableObject {
-  @Published var imageData: Data?
-  
-  let urlString = Api.Picture.pictureList
-  
-  enum FetchError: Error {
-    case badRequest
-    case badJSON
-  }
-  
-  func fetchImage(picture_id: String) async
-  throws {
-    guard let url = URL(string: urlString + "\(picture_id)") else { return }
+    @Published var imageData: Data?
     
-    var request = URLRequest(url: url)
-    request.setValue("Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2MTFhYTk0M2EwYWQ4NzNhZGU0OTJkMSJ9.ccKyeJlInJ5Rs9QzuYktxhp5V61bc0iTOifaqaVvH2A", forHTTPHeaderField: "Authorization")
+    let urlString = Api.Picture.pictureList
     
-    let (data, response) = try await URLSession.shared.data(for: request)
-    guard (response as? HTTPURLResponse)?.statusCode == 200 else { throw FetchError.badRequest }
-
-    print(data)
-    imageData = data
-  }
+    enum FetchError: Error {
+        case badRequest
+        case badJSON
+    }
+    
+    func fetchImage(picture_id: String) async
+    throws {
+        guard let url = URL(string: urlString + "\(picture_id)") else { return }
+        
+        var request = URLRequest(url: url)
+        request.setValue("Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2MTFhYTk0M2EwYWQ4NzNhZGU0OTJkMSJ9.ccKyeJlInJ5Rs9QzuYktxhp5V61bc0iTOifaqaVvH2A", forHTTPHeaderField: "Authorization")
+        
+        let (data, response) = try await URLSession.shared.data(for: request)
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else { throw FetchError.badRequest }
+        
+        print(data)
+        DispatchQueue.main.async {
+            self.imageData = data
+        }
+    }
 }
