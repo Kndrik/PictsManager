6d4bf1387f5b71e053cd1369b2ee1b7f16b8c939
Stiaez <cesar-stevens.gonzalez-arango@epitech.eu>
2024-04-30 15:10:45 +0200

    fix: image orientation when capturing

--- PictsManager/Screen/Camera/CustomCameraView.swift
+++ PictsManager/Screen/Camera/CustomCameraView.swift
@@ -9,8 +9,9 @@ import SwiftUI
 
 struct CustomCameraView: View {
     
-    let cameraViewModel = CameraViewModel()
+    @StateObject var cameraViewModel = CameraViewModel()
     @Binding var capturedImage: UIImage?
+    @StateObject var orientationInfo = OrientationInfo()
     
     @Environment(\.presentationMode) private var presentationMode
     
@@ -21,6 +22,9 @@ struct CustomCameraView: View {
                 case .success(let photo):
                     if let data = photo.fileDataRepresentation() {
                         capturedImage = UIImage(data: data)
+                        if(orientationInfo.deviceOrientation.isPortrait){
+                            capturedImage = capturedImage?.rotate(radians: 0)
+                        }
                         presentationMode.wrappedValue.dismiss()
                         if let image = capturedImage {
                             cameraViewModel.uploadImage(paramName: "file", fileName: "taken_with_picts_manager.png", image: image)
@@ -80,3 +84,23 @@ struct CustomCameraView: View {
     }
 }
 
+
+extension UIImage {
+    func rotate(radians: CGFloat) -> UIImage? {
+        var newSize = CGRect(origin: CGPoint.zero, size: self.size).applying(CGAffineTransform(rotationAngle: CGFloat(radians))).size
+        newSize.width = floor(newSize.width)
+        newSize.height = floor(newSize.height)
+        UIGraphicsBeginImageContextWithOptions(newSize, false, self.scale)
+        let context = UIGraphicsGetCurrentContext()!
+
+        context.translateBy(x: newSize.width/2, y: newSize.height/2)
+        context.rotate(by: radians)
+        self.draw(in: CGRect(x: -self.size.width/2, y: -self.size.height/2, width: self.size.width, height: self.size.height))
+
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return newImage
+    }
+
+}
