c6a0ea99bd7aad17e78868a2ef1705cb535c39e4
minhduc <ducbv1407@gmail.com>
2024-05-02 00:54:36 +0200

    add: init photo search

--- PictsManager/Screen/Image/View/SearchView.swift
+++ PictsManager/Screen/Image/View/SearchView.swift
@@ -9,24 +9,34 @@ import SwiftUI
 
 struct Search: View {
     @EnvironmentObject var albumsViewModel: AlbumsViewModel
-    @State var album: Album?
-    @State var searchResults: [Album] = []
+    @State var searchResults: [SearchResult] = []
+    @State var filteredPhotos: [Photo] = []
     @State private var searchText = ""
     
     var body: some View {
         NavigationView {
-            ZStack {
-                List(searchResults, id: \.self) { album in
-                    NavigationLink(destination: AlbumPhotosView(album: album)) {
-                        AlbumPreview(album: album, isFavorite: false)
+            List {
+                ForEach(searchResults, id: \.id) { result in
+                    switch result {
+                    case .album(let album):
+                        NavigationLink(destination: AlbumPhotosView(album: album)) {
+                            AlbumPreview(album: album, isFavorite: false)
+                        }
+                    case .photo(let photo):
+                        Text(photo.filename)
+//                        NavigationLink(destination: ImageDetails(photo: photo, photos: filter)) {
+//                            Text(photo.filename)
+//                        }
                     }
                 }
-                .listStyle(PlainListStyle())
             }
+            .listStyle(PlainListStyle())
             .navigationTitle("Rechercher")
             .searchable(text: $searchText)
             .onChange(of: searchText) { _ in
+                searchResults = []
                 searchAlbums()
+                searchPhotos()
             }
         }
         .task {
@@ -40,11 +50,36 @@ struct Search: View {
     
     private func searchAlbums() {
         if !searchText.isEmpty {
-            searchResults = albumsViewModel.albumsData?.albums.filter {
-                $0.title.localizedCaseInsensitiveContains(searchText)
-            } ?? []
-        } else {
-            searchResults = []
+            if let albumsData = albumsViewModel.albumsData {
+                let filteredAlbums = albumsData.albums.filter { $0.title.localizedCaseInsensitiveContains(searchText) }
+                searchResults.append(contentsOf: filteredAlbums.map { .album($0) })
+            }
+        }
+    }
+    
+    private func searchPhotos() {
+        if !searchText.isEmpty {
+            var results = [SearchResult]()
+            for album in albumsViewModel.albumsData?.albums ?? [] {
+                let filteredPhotos = album.pictures.filter { $0.filename.localizedCaseInsensitiveContains(searchText) }
+                results.append(contentsOf: filteredPhotos.map { .photo($0) })
+            }
+            searchResults.append(contentsOf: results)
+        }
+    }
+}
+
+
+enum SearchResult: Identifiable {
+    case album(Album)
+    case photo(Photo)
+    
+    var id: String {
+        switch self {
+        case .album(let album):
+            return album.id ?? UUID().uuidString
+        case .photo(let photo):
+            return photo.id
         }
     }
 }
